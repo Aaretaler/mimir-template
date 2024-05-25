bdd2b653e164cb5b8e233a5a8c7f3a56b631d66b
Reto Kunz <reto.kunz@ost.ch>
2024-05-25 21:12:15 +0200

    game page mockup design added

--- src/pages/gamePage/GamePage.tsx
+++ src/pages/gamePage/GamePage.tsx
@@ -1,26 +1,31 @@
-import { useState } from "react";
-import { Button } from "../../components/Button";
-import { TextInput } from "../../components/TextInput";
+import { useState } from 'react'
+import { Button } from '../../components/Button'
+import { TextInput } from '../../components/TextInput'
 import styles from './GamePage.module.css'
 
 export const GamePage = () => {
-    let [back, setBack] = useState('')
-    let [front, setFront] = useState('')
+  let [back, setBack] = useState('')
+  let [front, setFront] = useState('')
 
-const handleEditClick = () => {
+  const handleEditClick = () => {
     // Hier kannst du die Logik einfügen, die beim Klick auf den Button ausgeführt werden soll.
-    console.log("Edit button clicked!");
-};
+    console.log('Edit button clicked!')
+  }
 
-return (
-    <div className={styles.cardEditor}>
-        <div className={styles.TableHeader}>Front</div>
-        <div className={styles.TableHeader}>Back</div>
-        <div />
-        <TextInput placeholder="Front" value={front} onChange={setFront} />
-        <TextInput placeholder="Back" value={back} onChange={setBack} />
-        <Button title="Edit" clickHandler={handleEditClick} />
-    </div>
+  return (
+    <>
+      <div className={styles.gameState}>
+        <div className={styles.TableHeader}>Progress</div>
+        <Button title="Delete" clickHandler={() => null}></Button>
+      </div>
 
-)
+      <div className={styles.cardContainer}>
+        <div className={styles.card}>Card</div>
+        <div className={styles.answerContainer}>
+          <TextInput placeholder="Placeholder" value={back} onChange={setBack} />
+          <Button title="Submit" clickHandler={handleEditClick} />
+        </div>
+      </div>
+    </>
+  )
 }
