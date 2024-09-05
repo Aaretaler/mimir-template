424a4546f0732e203a8d2fb48338d967e70affe9
Reto Kunz <reto.kunz@ost.ch>
2024-09-06 00:15:33 +0200

    Add login & logout buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import { ResultPage } from './pages/ResultPage/ResultPage.tsx'
 import { CardEditor } from './pages/cardEditor/cardEditor.tsx'
 import { CardManager } from './pages/cardManager/CardManager.tsx'
 import { GamePage } from './pages/gamePage/GamePage.tsx'
-
+import { LoginPage } from './pages/loginPage/loginPage.tsx'
 
 function App() {
   console.log('App component rendered')
   return (
+    // TODO route all paths to login page when not logged in
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<GamePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/result" element={<ResultPage />}></Route>
         <Route path="/cards" element={<CardManager />}></Route>
         <Route path="/edit/:id" element={<CardEditor />}></Route>
