2c7e9e9fc6f81fc5b6f8a5b48d42ccb42046b3d4
Christoph Strube <meister.strube@gmail.com>
2024-09-19 00:28:26 +0200

    responsive design added for all pages

--- src/pages/cardManager/TableRow.tsx
+++ src/pages/cardManager/TableRow.tsx
@@ -15,27 +15,25 @@ export const TableRow = ({
   handleEditButtonClick,
 }: Props) => (
   <Fragment key={item.id}>
-      <div className={styles.gridblock}>
-    <div className={styles.itemText1}>{item.front}</div>
-    <div className={styles.itemText2}>{item.back}</div>
-
-      <div className={styles.itemButton1}>
-      <Button
-        title="Edit"
-        clickHandler={() => {
-          handleEditButtonClick(item.id)
-        }}
-      />
-        </div>
-        <div className={styles.itemButton2}>
-      <Button
-        title="Delete"
-        clickHandler={() => {
-          handleDeleteButtonClick(item.id)
-        }}
-      />
+    <div className={styles.gridblockOuter}>
+      <div className={styles.gridblockText}>
+        <div className={styles.itemText1}>{item.front}</div>
+        <div className={styles.itemText2}>{item.back}</div>
       </div>
-      </div> 
-    
+      <div className={styles.gridblockButton}>
+        <Button
+          title="Edit"
+          clickHandler={() => {
+            handleEditButtonClick(item.id)
+          }}
+        />
+        <Button
+          title="Delete"
+          clickHandler={() => {
+            handleDeleteButtonClick(item.id)
+          }}
+        />
+      </div>
+    </div>
   </Fragment>
 )
