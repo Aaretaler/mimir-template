d41e3c7af02cbfa430bbbbb0614f77a9940993b0
Reto Kunz <reto.kunz@ost.ch>
2024-09-29 12:00:32 +0200

    REFACTOR: Style changes on result page

--- src/pages/ResultPage/ResultPage.tsx
+++ src/pages/ResultPage/ResultPage.tsx
@@ -55,7 +55,9 @@ export const ResultPage = () => {
           <div key={card.front} className={styles.resultRow}>
             <div className={styles.resultCell}>{card.front}</div>
             <div className={styles.resultCell}>{card.back}</div>
-            <div className={styles.resultCell + " " + (game.answers[index]?.toLowerCase() == card.back.toLowerCase() ? styles.right : styles.wrong)}>{game.answers[index]}</div>
+            <div className={styles.resultCell + " " + (game.answers[index]?.toLowerCase() == card.back.toLowerCase() ? styles.right : styles.wrong)}>
+              {game.answers[index] ? game.answers[index] : "No Answer given"}
+            </div>
             <div className={styles.resultCell  + " " + styles.hidden}>
               {game.answers[index]?.toLowerCase() == card.back.toLowerCase() ? (
                 <>&#x2713;</>
